606a90c481c9bc2a7845b245d0d00607e192b7f3
namevug <ciavug@gmail.com>
2023-10-03 22:59:06 -0300

    feat: Selecting Columns

--- src/users.tsx
+++ src/users.tsx
@@ -3,7 +3,7 @@ import { List, SimpleList, Datagrid, TextField, EmailField } from "react-admin";
 
 export const UserList = () => {
     const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"))
-    
+
     return (
         <List>
             {isSmall ? (
@@ -15,10 +15,7 @@ export const UserList = () => {
             ) : (
                 <Datagrid rowClick="edit">
                     <TextField source="id" />
-                    <TextField source="name" />
-                    <TextField source="username" />
                     <EmailField source="email" />
-                    <TextField source="address.street" />
                     <TextField source="phone" />
                     <TextField source="website" />
                     <TextField source="company.name" />
